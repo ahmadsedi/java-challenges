b14e603b5bddb47d1deecd986769ec27efe82563
ahmadseddighi@gmail.com <ahmadseddighi@gmail.com>
2022-07-02 23:34:57 +0100

    Add trimTo10 and createMap methods

--- src/main/java/com/ahmadsedighi/java/challenges/collections/CollectionsUtility.java
+++ src/main/java/com/ahmadsedighi/java/challenges/collections/CollectionsUtility.java
@@ -1,10 +1,9 @@
 package com.ahmadsedighi.java.challenges.collections;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author Ahmad R. Seddighi (ahmadseddighi@yahoo.com)
@@ -20,5 +19,26 @@ public class CollectionsUtility {
     public static <E> Set<E> mergeDistinct(List<E>...lists){
         return Arrays.stream(lists).flatMap(Collection::stream).collect(Collectors.toSet());
     }
+
+    public static List<String> mergeAndRemoveEmpty(List<String>...lists){
+        List<String> list = merge(lists);
+        list.removeIf(String::isEmpty);
+        return list;
+    }
+
+    public static List<String> trimTo10(List<String> list, int n){
+        List<String> newList = new ArrayList(list);//list might be immutable, so we need to make a copy
+        newList.replaceAll(e->e.length()>n?e.substring(0, n):e);
+        return newList;
+    }
+
+    public static <E, F> Map<E, F> createMap(Set<E> keys, List<F> values){
+        if(keys.size()!=values.size()){
+            throw new IllegalArgumentException("Set and List must have same size!");
+        }
+        List<E> keysList = new ArrayList<E>(keys);
+        return Map.ofEntries(Stream.iterate(0, i->i+1).limit(keys.size()).map(i->Map.entry(keysList.get(i), values.get(i))).toArray(Map.Entry[]::new));
+    }
+
 
 }
--- src/test/java/com/ahmadsedighi/java/challenges/CollectionsUtilityTest.java
+++ src/test/java/com/ahmadsedighi/java/challenges/CollectionsUtilityTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static com.ahmadsedighi.java.challenges.collections.CollectionsUtility.*;
@@ -63,5 +64,40 @@ public class CollectionsUtilityTest {
     public void mergeDistinct_twoListsWithDuplicates_returnMerged(){
         Assertions.assertEquals(Set.of("Java", "Programming", "Language"),
                 mergeDistinct(List.of("Java", "Programming", "Language"), List.of("Programming", "Language")));
+    }
+
+    @Test
+    public void mergeAndRemoveEmpty_notIncludedEmpty_returnSame(){
+        Assertions.assertEquals(List.of("Java"), mergeAndRemoveEmpty(List.of("Java")));
+    }
+
+    @Test
+    public void mergeAndRemoveEmpty_includedEmpty_returnWithoutEmpty(){
+        Assertions.assertEquals(List.of("Java"), mergeAndRemoveEmpty(List.of("Java", "", "")));
+    }
+
+    @Test
+    public void trimTo10_givenEmptyList_returnEmpty(){
+        Assertions.assertEquals(List.of(), trimTo10(List.of(), 10));
+    }
+
+    @Test
+    public void trimTo10_givenNormalList_returnProcessed(){
+        Assertions.assertEquals(List.of("Java Progr"), trimTo10(List.of("Java Programming Language"), 10));
+    }
+
+    @Test
+    public void createMap_givenDiffSize_exceptionExpected(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->createMap(Set.of(""), List.of()));
+    }
+
+    @Test
+    public void createMap_givenEmpty_returnEmpty(){
+        Assertions.assertEquals(Map.of(), createMap(Set.of(), List.of()));
+    }
+
+    @Test
+    public void createMap_givenNormal_returnExpected(){
+        Assertions.assertEquals(Map.of("Language", "Java"), createMap(Set.of("Language"), List.of("Java")));
     }
 }
